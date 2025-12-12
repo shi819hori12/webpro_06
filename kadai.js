831b2c986397b0b4ef79b06e7cd11faff47d5045
詩穂里 <shiho@shihosatonoMacBook-Pro.local>
2025-12-12 15:24:54 +0900

    k-pop更新したよ

--- kadai.js
+++ kadai.js
@@ -5,20 +5,20 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
-let station2 = [
-  { id:1,  name:"東京駅", change:"総武本線，中央線，etc", passengers:403831, distance:0 },
-  { id:2,  name:"八丁堀駅", change:"日比谷線", passengers:31071, distance:1.2 },
-  { id:3,  name:"新木場駅", change:"有楽町線，りんかい線", passengers:67206, distance:7.4 },
-  { id:4,  name:"舞浜駅", change:"舞浜リゾートライン", passengers:76156,distance:12.7 },
-  { id:5,  name:"新習志野駅", change:"", passengers:11655, distance:28.3 },
-  { id:6,  name:"千葉みなと駅", change:"千葉都市モノレール", passengers:16602, distance:39.0 },
-  { id:7,  name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
+let kpop_data = [
+  { id:1,  name:"TWICE", member:"ナヨン，ジョンヨン，モモ，サナ，ジヒョ，ミナ，ダヒョン，チェヨン，ツウィ", fanname:"ONCE"},
+  { id:2,  name:"BTS", member:"ナムジュン，ジン，ユンギ，ホソク，ジミン，テヒョン，ジョングク", fanname:"ARMY"},
+  { id:3,  name:"TREASURE", member:"ヒョンソク，ジフン，ヨシ，ジュンギュ，ジェヒョク，アサヒ，ドヨン，ハルト，ジョンウ，ジョンファン", fanname:"TREASURE Maker"},
+  { id:4,  name:"&TEAM", member:"ウィジュ，ケイ，フウマ，ニコラス，ユウマ，ジョウ，ハルア，タキ，マキ", fanname:"LUNE"},
+  { id:5,  name:"BABYMONSTER", member:"ルカ，ファリタ，アヒョン，アサ，ラミ，ローラ，チキータ", fanname:"MONSTIEZ"},
+  { id:6,  name:"IZ*ONE", member:"ウンビ，サクラ，へウォン，イェナ，チェヨン，チェウォン，ミンジュ，ナコ，ヒトミ，ユリ，ユジン，ウォニョン", fanname:"WIZ*ONE"},
+  { id:7,  name:"NMIXX", member:"リリー，へウォン，ソリュン，ギュジン，ベイ，ジウ", fanname:"NSWER"},
 ];
 
 // 一覧
 app.get("/k-pop", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
-  res.render('k-pop', {data: station2} );
+  res.render('k-pop', {data: kpop_data} );
 });
 
 // Create
@@ -30,7 +30,7 @@ app.get("/k-pop/create", (req, res) => {
 app.get("/k-pop/:number", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
   const number = req.params.number;
-  const detail = station2[ number ];
+  const detail = kpop_data[ number ];
   res.render('k-pop_detail', {id: number, data: detail} );
 });
 
@@ -39,29 +39,27 @@ app.get("/k-pop/delete/:number", (req, res) => {
   // 本来は削除の確認ページを表示する
   // 本来は削除する番号が存在するか厳重にチェックする
   // 本来ならここにDBとのやり取りが入る
-  station2.splice( req.params.number, 1 );
+  kpop_data.splice( req.params.number, 1 );
   res.redirect('/k-pop' );
 });
 
 // Create
 app.post("/k-pop", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
-  const id = station2.length + 1;
-  const code = req.body.code;
+  const id = fannam.length + 1;
   const name = req.body.name;
-  const change = req.body.change;
-  const passengers = req.body.passengers;
-  const distance = req.body.distance;
-  station2.push( { id: id, code: code, name: name, change: change, passengers: passengers, distance: distance } );
-  console.log( station2 );
-  res.render('k-pop', {data: station2} );
+  const member = req.body.member;
+  const fanname = req.body.fanname;
+  kpop_data.push( { id: id, name: name, member: member, fanname: fanname} );
+  console.log( kpop_data );
+  res.render('k-pop', {data: kpop_data} );
 });
 
 // Edit
 app.get("/k-pop/edit/:number", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
   const number = req.params.number;
-  const detail = station2[ number ];
+  const detail = kpop_data[ number ];
   res.render('k-pop_edit', {id: number, data: detail} );
 });
 
@@ -69,23 +67,13 @@ app.get("/k-pop/edit/:number", (req, res) => {
 app.post("/k-pop/update/:number", (req, res) => {
   // 本来は変更する番号が存在するか，各項目が正しいか厳重にチェックする
   // 本来ならここにDBとのやり取りが入る
-  station2[req.params.number].code = req.body.code;
-  station2[req.params.number].name = req.body.name;
-  station2[req.params.number].change = req.body.change;
-  station2[req.params.number].passengers = req.body.passengers;
-  station2[req.params.number].distance = req.body.distance;
-  console.log( station2 );
+  kpop_data[req.params.number].id = req.body.id;
+  kpop_data[req.params.number].name = req.body.name;
+  kpop_data[req.params.number].member = req.body.member;
+  kpop_data[req.params.number].fanname = req.body.fanname;
+  console.log( kpop_data );
   res.redirect('/k-pop' );
 });
-
-
-
-const express = require("express");
-const app = express();
-
-app.set('view engine', 'ejs');
-app.use("/public", express.static(__dirname + "/public"));
-app.use(express.urlencoded({ extended: true }));
 
 
 
